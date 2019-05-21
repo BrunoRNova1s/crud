e7e9d739282fc2592f1e8c3fdfee628c79fddd0a
Bruno Novais <bruno.br001@hotmail.com>
2019-05-21 10:32:55 +0100

    footer

--- front-end/src/components/template/Footer.jsx
+++ front-end/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Footer
-    </footer>
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>BR</strong>uno
+        </span>
+    </footer>   
