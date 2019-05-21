6f81504f8fd0187f3ff0d7b72846feb28f563d85
Bruno Novais <bruno.br001@hotmail.com>
2019-05-21 11:11:28 +0100

    nav item css

--- front-end/src/components/template/Nav.jsx
+++ front-end/src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Pode criar se um componente - como foi feito no header */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Utilizadores
+            </a>
+        </nav>
     </aside>
