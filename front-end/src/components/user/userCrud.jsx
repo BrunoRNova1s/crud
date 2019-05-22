80720c07b3f58e29487bc5320c21f81e4e82877a
Bruno Novais <bruno.br001@hotmail.com>
2019-05-22 11:47:33 +0100

    userCrud form

--- front-end/src/components/user/userCrud.jsx
+++ front-end/src/components/user/userCrud.jsx
@@ -41,13 +41,80 @@ export default class UserCrud extends Component {
   }
 
   getUpdatedList(user) {
-      //como filter cria uma nova lista, não é preciso clonar a do state
-      const list = this.state.list.filter(u => u.id !== user.id)
-      list.unshift(user)
-      return list
+    //como filter cria uma nova lista, não é preciso clonar a do state
+    const list = this.state.list.filter(u => u.id !== user.id);
+    list.unshift(user);
+    return list;
+  }
+
+  updateField(event) {
+    //clonar o utilizador - clonar objeto, alterar -
+    //depois utilizar o setState para definir um novo estado
+    const user = { ...this.state.user };
+    //valor que está dentro do campo input
+    //[] - está a utilizar a notação string e não "."
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  }
+
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.user.name}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o nome..."
+              />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>E-mail</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                value={this.state.user.email}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o e-mail..."
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 d-flex justify-content">
+              <button className="btn btn-primary" 
+                onClick={e => this.save(e)}
+              >
+                Guardar
+              </button>
+
+              <button
+                className="btn btn-secondary ml-2"
+                onClick={e => this.clear(e)}
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
-    return <Main {...headerProps}>Registo de Utilizador</Main>;
+    return (
+      <Main {...headerProps}>
+        {this.renderForm()}
+      </Main>
+    )
   }
 }
